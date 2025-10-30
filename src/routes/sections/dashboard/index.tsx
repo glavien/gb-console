6ef2f14689f48028f760012517c73ed4607235aa
cybershock1 <pavicaes@gmail.com>
2025-10-30 18:36:26 +0200

    fix: Fixed linter errors

--- src/routes/sections/dashboard/index.tsx
+++ src/routes/sections/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { GLOBAL_CONFIG } from "@/global-config";
 import DashboardLayout from "@/layouts/dashboard";
-import LoginAuthGuard from "@/routes/components/login-auth-guard";
+// import LoginAuthGuard from "@/routes/components/login-auth-guard";
 import { Navigate, type RouteObject } from "react-router";
 import { backendDashboardRoutes } from "./backend";
 import { frontendDashboardRoutes } from "./frontend";
